2dc146c9fb42c26eae8af95dcd337a40e0fbb701
Ritik <sadhritik6@gmail.com>
2026-08-21 18:30:40 +0530

    updated navbar with react-icons

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,7 @@ import Navbar from "./components/Navbar";
 function App() {
   return (
     <>
-    <h1 className="text-4xl font-bold text-red-500">
-  Aurevia 💎
-</h1>
+    
     <Navbar/>
     </>
   );
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,17 +1,29 @@
+import { FiSearch } from "react-icons/fi";
+import { FiUser } from "react-icons/fi";
+import { FiHeart } from "react-icons/fi";
+import { FiShoppingBag } from "react-icons/fi";
+
 function Navbar() {
   return (
-    <nav className="flex justify-between items-center px-7 py-5 border-b">
-      <h1>Aurevia</h1>
+    <nav className="flex justify-between items-center px-7 py-6 border-b shadow-sm">
+      <h1 className="text-2xl font-bold tracking-wider text-[#8B5E3C]">Aurevia</h1>
 
       <ul className="flex gap-8">
-        <li className="cursor-pointer text-sm hover:text-gray-500">Home</li>
-        <li className="cursor-pointer text-sm hover:text-gray-500">Jewellery</li>
-        <li className="cursor-pointer text-sm hover:text-gray-500">Collections</li>
-        <li className="cursor-pointer text-sm hover:text-gray-500">About</li>
-        <li className="cursor-pointer text-sm  hover:text-gray-500" >Contact</li>
+        <li className="cursor-pointer text-sm font-medium hover:text-gray-500 transition-colors duration-200">Home</li>
+        <li className="cursor-pointer text-sm font-medium hover:text-gray-500 transition-colors duration-200">Jewellery</li>
+        <li className="cursor-pointer text-sm font-medium hover:text-gray-500 transition-colors duration-200">Collections</li>
+        <li className="cursor-pointer text-sm font-medium hover:text-gray-500 transition-colors duration-200">About</li>
+        <li className="cursor-pointer text-sm font-medium hover:text-gray-500 transition-colors duration-200" >Contact</li>
       </ul>
+
+      <div className="flex items-center  gap-5">
+        <FiSearch className="cursor-pointer hover:text-gray-500" />
+        <FiUser className="cursor-pointer hover:text-gray-500" />
+        <FiHeart className="cursor-pointer hover:text-gray-500" />
+        <FiShoppingBag className="cursor-pointer hover:text-gray-500" />
+      </div>
+
     </nav>
   );
 }
-
 export default Navbar;
