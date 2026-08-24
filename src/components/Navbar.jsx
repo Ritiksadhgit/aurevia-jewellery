96847b99a3db7b038cfb26885738e8ce2f85ab1d
Ritik <sadhritik6@gmail.com>
2026-08-24 18:26:45 +0530

    Modofied responsive navBar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,14 +1,25 @@
-import { FiSearch } from "react-icons/fi";
-import { FiUser } from "react-icons/fi";
-import { FiHeart } from "react-icons/fi";
-import { FiShoppingBag } from "react-icons/fi";
+// import { FiSearch } from "react-icons/fi";
+// import { FiUser } from "react-icons/fi";
+// import { FiHeart } from "react-icons/fi";
+// import { FiShoppingBag } from "react-icons/fi";
+import { useState } from "react";
+import {
+  FiSearch,
+  FiUser,
+  FiHeart,
+  FiShoppingBag,
+  FiMenu,
+  FiX
+} from "react-icons/fi";
 
 function Navbar() {
+  const [menuOpen, setMenuOpen] = useState(false);
   return (
-    <nav className="flex justify-between items-center px-7 py-6 border-b shadow-sm">
+
+<nav className="flex justify-between items-center px-6 md:px-12 py-6 border-b shadow-sm">
       <h1 className="text-2xl font-bold tracking-wider text-[#8B5E3C]">Aurevia</h1>
 
-      <ul className="flex gap-8">
+      <ul className="hidden md:flex gap-8">
         <li className="cursor-pointer text-sm font-medium hover:text-gray-500 transition-colors duration-200">Home</li>
         <li className="cursor-pointer text-sm font-medium hover:text-gray-500 transition-colors duration-200">Jewellery</li>
         <li className="cursor-pointer text-sm font-medium hover:text-gray-500 transition-colors duration-200">Collections</li>
@@ -16,12 +27,31 @@ function Navbar() {
         <li className="cursor-pointer text-sm font-medium hover:text-gray-500 transition-colors duration-200" >Contact</li>
       </ul>
 
-      <div className="flex items-center  gap-5">
+      <div className="hidden md:flex items-center gap-5">
         <FiSearch className="cursor-pointer hover:text-gray-500 transition-colors duration-200" />
         <FiUser className="cursor-pointer hover:text-gray-500 transition-colors duration-200" />
         <FiHeart className="cursor-pointer hover:text-gray-500 transition-colors duration-200" />
         <FiShoppingBag className="cursor-pointer hover:text-gray-500 transition-colors duration-200" />
       </div>
+
+    
+         <button className="md:hidden text-2xl ml-auto"  onClick={() => setMenuOpen(!menuOpen)}>
+   
+          {menuOpen ? <FiX /> : <FiMenu />}
+
+          </button>
+
+            {menuOpen && (
+           <div className="md:hidden border-t px-6 py-5 bg-white shadow-sm">
+            <ul className="flex flex-col items-center gap-5">
+               <li>Home</li>
+              <li>Jewellery</li>
+             <li>Collections</li>
+             <li>About</li>
+             <li>Contact</li>
+           </ul>
+          </div>
+                 )}
 
     </nav>
   );
