374881c2834a945154cc88807a9ee740db3dda59
Ritik <sadhritik6@gmail.com>
2026-08-28 10:35:23 +0530

    created jewellery.jsx in jewellery folder

---
+++ src/pages/Jewellery/Jewellery.jsx
@@ -0,0 +1,15 @@
+function Jewellery() {
+  return (
+    <section className="py-20 px-6">
+      <h1 className="text-4xl font-bold text-center text-[#8B5E3C]">
+        Jewellery Collection
+      </h1>
+
+      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-8 mt-12">
+  
+     </div> 
+    </section>
+  );
+}
+
+export default Jewellery;
